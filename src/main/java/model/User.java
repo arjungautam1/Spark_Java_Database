18df8300f61e8600e0bb00f0ad27cdd0adbdd508
arjun-sudo <arjungautam5431@gmail.com>
2021-02-17 14:29:07 +0545

    model

---
+++ src/main/java/model/User.java
@@ -0,0 +1,56 @@
+/**
+ * @author : Arjun Gautam
+ * e-mail : arjungautam5431@gmail.com
+ * Date :2021-02-17
+ * Time :14:27
+ */
+package model;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public User(int id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
